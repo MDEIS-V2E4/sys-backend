0b1c0ccd47a8179b1de0dacacf009f853c7d5cb9
enunez <enunez.dev@gmail.com>
2025-03-11 23:57:03 -0400

    update index

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(logger());
 
-app.use('/v1/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Bienvenido');
 });
 
